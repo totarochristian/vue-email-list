ec135b5bb3a34c33c1ab2877bcd09c3527866204
totarochristian <totaro.christian.18@gmail.com>
2023-04-11 16:32:36 +0200

    Fix problems with the showing of the mails

--- js/script.js
+++ js/script.js
@@ -13,24 +13,24 @@ createApp({
     /**
      * Method used to return a random mail
      */
-    GetSingleRandomMail(){
-      axios.get(this.apiRestUri).then((res)=>{
-        if(res.data.success)
-          return res.data.response;
-        return "";
-      });
+    async GetSingleRandomMail(){
+      const res = await axios.get(this.apiRestUri);
+      if(res.data.success)
+        return res.data.response;
+      return "";
     },
     ResetGeneratedMails(){
       this.generatedMails.splice(0,this.generatedMails.length);
     },
-    GenerateMails(){
+    async GenerateMails(){
       this.ResetGeneratedMails();
       //Generate mails and push to the generatedMails array
-      for(let i=0; i<this.maxNumberOfMails; i++)
-        this.generatedMails.push(this.GetSingleRandomMail());
+      for(let i=0; i<this.maxNumberOfMails; i++){
+        this.generatedMails.push(await this.GetSingleRandomMail());
+      }
     }
   },
-  mounted(){
+ mounted(){
     this.GenerateMails();
   }
 }).mount("#app");
