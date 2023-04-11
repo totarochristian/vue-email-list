b160e1390b71c8b8664309530ce354999b2678c7
totarochristian <totaro.christian.18@gmail.com>
2023-04-11 15:35:07 +0200

    Add a parm of the createApp with the api rest uri used to get the randomic mails

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
   data(){
     return {
-      title: "Lista di e-mail randomiche"
+      title: "Lista di e-mail randomiche",
+      apiRestUri: "https://flynn.boolean.careers/exercises/api/random/mail"
     }
   },
   methods: {
