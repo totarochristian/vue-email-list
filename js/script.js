8f0c4a022b56cf5d1126c0618b9a050b31300778
totarochristian <totaro.christian.18@gmail.com>
2023-04-11 15:31:05 +0200

    Define the title in the createApp data

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ const { createApp } = Vue;
 createApp({
   data(){
     return {
-
+      title: "Lista di e-mail randomiche"
     }
   },
   methods: {
 
   },
   mounted(){
-    
+
   }
 }).mount("#app");
